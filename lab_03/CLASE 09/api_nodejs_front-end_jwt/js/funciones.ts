03107ed7fa5ef748ecd213802f9e8996332e188c
Nahuel <36241474+nahuel812@users.noreply.github.com>
2022-11-11 11:43:24 -0300

    Add files via upload

---
+++ lab_03/CLASE 09/api_nodejs_front-end_jwt/js/funciones.ts
@@ -0,0 +1,13 @@
+
+const URL_API : string = "http://localhost:9876/";
+const URL_BASE : string = "http://localhost/lab_3/api_nodejs_front-end_jwt/";
+//PONER EL LINK DE NUESTRO FRONT EN EL htdocs
+
+function ArmarAlert(mensaje:string, tipo:string = "success"):string
+{
+    let alerta:string = '<div id="alert_' + tipo + '" class="alert alert-' + tipo + ' alert-dismissable">';
+    alerta += '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+    alerta += '<span class="d-inline-block text-truncate" style="max-width: 450px;">' + mensaje + ' </span></div>';
+
+    return alerta;
+}
